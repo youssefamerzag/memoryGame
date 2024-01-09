1435d8da43ce5b1e01370623a2849f5ef29783c9
youssef <amerzag.youssef4@gmail.com>
2024-01-09 15:39:52 +0100

    version 1 without CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+
+  const imgs = [
+    {id : 0 , key : 0 , src : 'imgs/html.png' , disponible : true},
+    {id : 1 , key : 1 , src : 'imgs/mongodb.png', disponible : true},
+    {id : 1 , key : 2 , src : 'imgs/nodejs.png', disponible : true},
+    {id : 3 , key : 3 , src : 'imgs/reactjs.png', disponible : true},
+    {id : 0 , key : 4 , src : 'imgs/html.png', disponible : true},
+    {id : 1 , key : 5 , src : 'imgs/mongodb.png', disponible : true},
+    {id : 2 , key : 6 , src : 'imgs/nodejs.png', disponible : true},
+    {id : 3 , key : 7 , src : 'imgs/reactjs.png', disponible : true},
+  ]
+
+  const [firstChoosingImg , setFirstChoosingImg] = useState()
+  const [secondeChoosingImg , setsecondeChoosingImg] = useState()
+
+  const imgRef = useRef(imgs.map(() => React.createRef()))
+
+  const choose = (imgKey) => {
+    const selectedImg = imgs.find(img => img.key === imgKey);
+    if(selectedImg.disponible){
+      if(firstChoosingImg === undefined){
+        setFirstChoosingImg(selectedImg.id)
+        imgRef.current[imgKey].current.style.backgroundColor = 'lightblue';
+      }else if (secondeChoosingImg === undefined){
+        setsecondeChoosingImg(selectedImg.id)
+      }
+    }
+  };
+  
+
+  useEffect((imgKey) => {
+    if( firstChoosingImg !== undefined &&  secondeChoosingImg !== undefined ){
+      if(firstChoosingImg === secondeChoosingImg ) {
+        alert('done')
+        imgRef.current[imgKey].current.style.backgroundColor = 'green';
+        setFirstChoosingImg()
+        setsecondeChoosingImg()
+      }else if(firstChoosingImg != secondeChoosingImg || firstChoosingImg != firstChoosingImg) {
+        alert('false')
+        imgRef.current[imgKey].current.style.backgroundColor = 'null';
+        setFirstChoosingImg()
+        setsecondeChoosingImg()
+      }
+    }
+  },[firstChoosingImg, secondeChoosingImg])
+
+  return(
+    <div>
+      <div>
+        {imgs.map(img => 
+            <img ref={imgRef.current[img.key]} src={img.src} id={img.id} key={img.key} onClick={() => choose(img.key)} ></img>
+          )}
+      </div>
     </div>
-  );
+  )
 }
-
 export default App;
